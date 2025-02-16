a8288dd6e7258326b80de6ecf23cc16f91b67940
elen-purple <anna.havrylova.2011@gmail.com>
2025-02-16 12:13:09 +0200

    makes filter

---
+++ js/filter.js
@@ -0,0 +1,451 @@
+const petArray = [
+  {
+    name: "Luna",
+    price: 40,
+    info: {
+      type: "cat",
+      gender: "girl",
+      skin: "red",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0Zuywj8gFc3iM-gvaJfAEXranyDTGrXQx2g&s",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Jack",
+    price: 36,
+    info: {
+      type: "dog",
+      gender: "boy",
+      skin: "black",
+      image:
+        "https://zoobonus.ua/storage/breeds/images/big/t5jnpkELPD4QWuAHDzqsqVOqDqXiC8qrWPY9KF3s.jpg",
+      age: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Magy",
+    price: 50,
+    info: {
+      type: "turtle",
+      gender: "girl",
+      skin: "gold",
+      image:
+        "https://facts.cx.ua/uploads/facts-2/mini/cikavi-fakti-pro-morskih-cherepah-900-600.jpg",
+      age: 3,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Lucie",
+    price: 34,
+    info: {
+      type: "rabbit",
+      gender: "girl",
+      skin: "white",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFsouVYTbNORB9Ogjhn8Zz_ISrQ7U9SLsmwQ&s",
+      age: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Kuko",
+    price: 47,
+    info: {
+      type: "parrot",
+      gender: "boy",
+      skin: "green",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwQqlnTZ-K8sl9wTNwdzO-NndZf0GrI8bPUQ&s",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "George",
+    price: 41,
+    info: {
+      type: "cat",
+      gender: "boy",
+      skin: "brown",
+      image:
+        "https://cdn.riastatic.com/photosnew/general/adv_photo/shotlandska-kishechka-vid-titulovanih-batkiv-malenki-vushka__18132244bt.jpg",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Becky",
+    price: 31,
+    info: {
+      type: "dog",
+      gender: "girl",
+      skin: "gold",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFlx6uD4tPy0Ee2jmV3050_E04Zv84iM7bfA&s",
+      age: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Gresy",
+    price: 50,
+    info: {
+      type: "turtle",
+      gender: "girl",
+      skin: "orange",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbBapn76NaXHKu5fNs272VsmU8nTUB5pjBkg&s",
+      age: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Joiky",
+    price: 44,
+    info: {
+      type: "hamster",
+      gender: "girl",
+      skin: "grey-white",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCYHl_tQA30ydejTASYuJzfQFX3S7LAZndWA&s",
+      age: 3,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Rosy",
+    price: 43,
+    info: {
+      type: "parrot",
+      gender: "girl",
+      skin: "red",
+      image:
+        "https://media.istockphoto.com/id/1358208752/photo/macaw-parrot-flying-in-dark-green-vegetation-with-beautiful-back-light-and-rain-scarlet-macaw.jpg?s=612x612&w=0&k=20&c=3396GlVCOOsK34dEUfjCNqsDFPOnURw8kQC76wQaKY8=",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Goldness",
+    price: 61,
+    info: {
+      type: "snake",
+      gender: "girl",
+      skin: "gold",
+      image:
+        "https://www.evolutionreptiles.co.uk/wp-content/uploads/2019/05/Gold-dust-corn-snake-600x540.jpg",
+      age: 5,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Windy",
+    price: 47,
+    info: {
+      type: "fenix fox",
+      gender: "boy",
+      skin: "gold",
+      image:
+        "https://www.endangeredwolfcenter.org/wp-content/uploads/2021/11/MJS_5861-Daisy_3-1.jpg.webp",
+      age: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Deer",
+    price: 47,
+    info: {
+      type: "turtle",
+      gender: "boy",
+      skin: "green",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShRrjaNDnoEt4kYLkxDictH1ZR7Hu9S1gLKQ&s",
+      age: 6,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Kiry",
+    price: 49,
+    info: {
+      type: "cat",
+      gender: "boy",
+      skin: "black",
+      image:
+        "https://mypetsays.co.uk/cdn/shop/articles/National_Black_Cat_Day.jpg?v=1695635944&width=1920",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Jemy",
+    price: 30,
+    info: {
+      type: "dog",
+      gender: "boy",
+      skin: "black",
+      image:
+        "https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/activities-fun/10-great-small-dog-breeds/maltese-portrait.jpg?h=448&w=740&hash=B111F1998758CA0ED2442A4928D5105D",
+      age: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Louis",
+    price: 43,
+    info: {
+      type: "rat",
+      gender: "boy",
+      skin: "black-white",
+      image:
+        "https://www.batterseaparkzoo.co.uk/media/bw1p0wt0/img_7849.jpg?anchor=center&rmode=crop&width=320&quality=80",
+      age: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Fancy",
+    price: 56,
+    info: {
+      type: "parrot",
+      gender: "girl",
+      skin: "grey",
+      image:
+        "https://media.npr.org/assets/img/2024/01/25/african-grey-parrot-a4f997180fb09d316d26d2c2b3465120fabec08a.jpg",
+      age: 4,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Figgy",
+    price: 71,
+    info: {
+      type: "parrot",
+      gender: "boy",
+      skin: "green-yellow",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUoyZ1R1LOxC4gQE4WX3p7U13jPVinoJyyQg&s",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Ore",
+    price: 47,
+    info: {
+      type: "rabbit",
+      gender: "girl",
+      skin: "black",
+      image:
+        "https://i.pinimg.com/736x/f7/68/91/f76891eb583fe99e4a9427b09b2c06a4.jpg",
+      age: 6,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Nency",
+    price: 37,
+    info: {
+      type: "fox",
+      gender: "girl",
+      skin: "red",
+      image:
+        "https://friendsofanimals.org/wp-content/uploads/2023/12/foxfb.png",
+      age: 3,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Jile",
+    price: 56,
+    info: {
+      type: "cat",
+      gender: "boy",
+      skin: "red-white",
+      image:
+        "https://static.scientificamerican.com/sciam/cache/file/2AE14CDD-1265-470C-9B15F49024186C10_source.jpg?w=1200",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Michel",
+    price: 45,
+    info: {
+      type: "cat",
+      gender: "boy",
+      skin: "white",
+      image:
+        "https://t4.ftcdn.net/jpg/01/16/19/69/360_F_116196948_GgNxAC3n2KFUp9GmxEXcGyGM4hZlwMv3.jpg",
+      age: 1,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Moby",
+    price: 34,
+    info: {
+      type: "dog",
+      gender: "girl",
+      skin: "brown-white",
+      image:
+        "https://img.freepik.com/premium-photo/dog-couch-sleep-relax-home-happy-pet-comfort-safety-living-room-tired-jack-russell-sleeping-couch-furniture-pets-with-loyalty-cute-face-pillow-lounge-alone_590464-193016.jpg?w=360",
+      age: 5,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+  {
+    name: "Jody",
+    price: 33,
+    info: {
+      type: "hamster",
+      gender: "boy",
+      skin: "red",
+      image:
+        "https://cdn.shopify.com/s/files/1/0424/4606/1723/files/Syrian-Hamster_grande.jpg?v=1594383125",
+      age: 2,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ipsa asperiores ipsam, quidem qui, distinctio, commodi exercitationem veniam odio tenetur eligendi quibusdam eos praesentium rem dolor minus necessitatibus aspernatur? Est recusandae tenetur rerum blanditiis ad? Illum fugit officia voluptatibus eligendi?",
+    },
+  },
+];
+
+const filter = document.querySelector("#filter");
+const petList = document.querySelector("#pet-list");
+const form = document.querySelector("#filter-form");
+const closeBtn = document.querySelector("#close-btn");
+const submitBtn = document.querySelector("#submit-btn");
+const resetBtn = document.querySelector("#reset-btn");
+
+const minPriceInput = document.querySelector("#min-price-input");
+const maxPriceInput = document.querySelector("#max-price-input");
+const typeInput = document.querySelector("#type-input");
+const skinInput = document.querySelector("#skin-input");
+const genderBtn = document.querySelector("#gender-btn");
+const genderList = document.querySelector("#gender-list");
+const minAgeInput = document.querySelector("#min-age-input");
+const maxAgeInput = document.querySelector("#max-age-input");
+
+let genderType = "default";
+let filteredPetArray = [...petArray];
+
+closeBtn.addEventListener("click", () => {
+  filter.classList.toggle("closed");
+  closeBtn.firstElementChild.classList.toggle("rotated");
+});
+
+genderBtn.addEventListener("click", () => {
+  genderList.classList.toggle("is-hidden");
+});
+
+genderList.addEventListener("click", (e) => {
+  genderList.classList.toggle("is-hidden");
+  if (e.target.dataset.gender === "default") {
+    genderType = "default";
+  } else if (e.target.dataset.gender === "boy") {
+    genderType = "boy";
+  } else if (e.target.dataset.gender === "girl") {
+    genderType = "girl";
+  }
+  genderBtn.textContent = genderType;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  filteredPetArray = [...petArray];
+  filteredPetArray =
+    minPriceInput.value !== ""
+      ? filteredPetArray.filter(
+          (pet) => pet.price >= Number.parseFloat(minPriceInput.value)
+        )
+      : petArray;
+  filteredPetArray =
+    maxPriceInput.value !== ""
+      ? filteredPetArray.filter(
+          (pet) => pet.price <= Number.parseFloat(maxPriceInput.value)
+        )
+      : filteredPetArray;
+  filteredPetArray =
+    typeInput.value !== ""
+      ? filteredPetArray.filter((pet) =>
+          pet.info.type.includes(typeInput.value.toLowerCase())
+        )
+      : filteredPetArray;
+  filteredPetArray =
+    skinInput.value !== ""
+      ? filteredPetArray.filter((pet) =>
+          pet.info.skin.includes(skinInput.value.toLowerCase())
+        )
+      : filteredPetArray;
+  filteredPetArray =
+    minAgeInput.value !== ""
+      ? filteredPetArray.filter(
+          (pet) => pet.info.age >= Number.parseFloat(minAgeInput.value)
+        )
+      : filteredPetArray;
+  filteredPetArray =
+    maxAgeInput.value !== ""
+      ? filteredPetArray.filter(
+          (pet) => pet.info.age <= Number.parseFloat(maxAgeInput.value)
+        )
+      : filteredPetArray;
+  filteredPetArray =
+    genderType !== "default"
+      ? filteredPetArray.filter((pet) => pet.info.gender === genderType)
+      : filteredPetArray;
+  rebuildPets(filteredPetArray);
+});
+
+resetBtn.addEventListener("click", () => {
+  minPriceInput.value = "";
+  maxPriceInput.value = "";
+  typeInput.value = "";
+  skinInput.value = "";
+  genderType = "default";
+  genderBtn.textContent = genderType;
+  minAgeInput.value = "";
+  maxAgeInput.value = "";
+  filteredPetArray = [...petArray];
+  rebuildPets(filteredPetArray);
+});
+
+function rebuildPets(list) {
+  petList.innerHTML = "";
+  if (list.length === 0) {
+    petList.insertAdjacentHTML(
+      "afterbegin",
+      `<p class="catalog__message">There aren't any pets like this</p>`
+    );
+  }
+  list.forEach((pet) => {
+    const {
+      name,
+      price,
+      info: { type, gender, skin, image, age },
+    } = pet;
+    petList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="catalog__item">
+        <div class="catalog__wrapper">
+            <img src="${image}" alt="${skin} ${type} called ${name}" class="catalog__img">
+        </div>
+        <div class="catalog__block">
+            <div class="catalog__header">
+                <h2 class="catalog__subtitle">${name}</h2>
+                <p class="catalog__price">${price}$</p>
+            </div>
+            <div class="catalog__info">
+                <p class="catalog__text">Type: <span class="catalog__span catalog__type">${type}</span></p>
+                <p class="catalog__text">Age: <span class="catalog__span catalog__age">${age}</span></p>
+                <p class="catalog__text">Gender: <span class="catalog__span catalog__gender">${gender}</span></p>
+                <p class="catalog__text">Skin: <span class="catalog__span catalog__skin">${skin}</span></p>
+            </div>
+            <button id="modal-btn" type="button" class="catalog__btn">See more</button>
+        </div>
+    </li>`
+    );
+  });
+}
